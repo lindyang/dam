cf253ccc169b1bec8a75e41f2b87d74a354eecc6
lindyang <yanglindong@jinchongzi.com>
2016-12-08 05:15:31 +0800

    2

--- ngx-runtime/3rd/http.lua
+++ ngx-runtime/3rd/http.lua
@@ -64,3 +64,216 @@ local co_wrap = function(func)
         end
     end
 end
+
+
+local _M = {
+    _VERSION = '0.08',
+}
+
+_M._USER_AGENT = "lua-resty-http/" .. M._VERSION .. " (Lua) ngx_lua/" .. ngx.config.ngx_lua_version
+
+local mt = {__index = _M}
+
+local HTTP = {
+    [1.0] = " HTTP/1.0\r\n",
+    [1.1] = " HTTP/1.1\r\n",
+}
+
+local DEFAULT_PARAMS = {
+    method = "GET",
+    path = "/",
+    version = 1.1,
+}
+
+function _M.new(self)
+    local sock, err = ngx_socket_tcp()
+    if not sock then
+        return  nil, err
+    end
+    return setmetatable({sock=sock, keepalive=true}, mt)
+end
+
+
+function _M.set_timeout(self, timeout)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    return sock:settimeout(timeout)
+end
+
+
+function _M.ssl_handshake(self, ...)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    self.ssl = true
+    return sock:sslhandshake(...)
+end
+
+
+function _M.connect(self, ...)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    self.host = select(1, ...)
+    self.port = select(2, ...)
+    -- If port is not a number, this is likely a unix domain socket connection.
+    if type(self.port) ~= "number" then
+        self.port = nil
+    end
+    self.keepalive = true
+    return sock.connect(...)
+end
+
+
+function _M.set_keepalive(self, ...)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    if self.keepalive == true then
+        return sock:setkeepalive(...)
+    else
+        -- The server said we must close the connection, so we cannot setkeepalive
+        -- If close() succeeds we return 2 instead of 1, to differentiate between
+        -- a normal setkeepalive() failure and an intentional close().
+        local res, err = sock:close()
+        if res then
+            return 2, "connection must be closed"
+        else
+            return res, err
+        end
+    end
+end
+
+
+function _M.get_reused_times(self)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    return sock:getreusedtimes()
+end
+
+
+function _M:close(self)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    return sock:close()
+end
+
+
+local function _should_receive_body(method, code)
+    if method == "HEAD" then return nil end
+    if code == 204 or code == 304 then return nil end
+    if code >= 100 and code < 200 then return nil end
+    return true
+end
+
+
+function _M.parse_uri(self, uri)
+    local m, err = ngx_re_match(uri, [[^(http[s]*)://([^:/]+)(?::(\d+))?(.*)]], "jo")
+    if not m then
+        if err then
+            return nil, "failed to match the uri: " .. err
+        end
+        return nil, "bad uri"
+    else
+        if m[3] then
+            m[3] = tonumber(m[3])
+        else
+            if m[1] == "https" then
+                m[3] = 443
+            else
+                m[3] = 80
+            end
+        end
+        if not m[4] or "" == m[4] then m[4] = "/" end
+        return m, nil
+    end
+end
+
+
+local function _format_request(params)
+    local version = params.version
+    local headers = params.headers or {}
+    local query = params.query or ""
+    if query then
+        if type(query) == "table" then
+            query = "?" .. ngx_encode_args(query)
+        end
+    end
+
+    -- Initialize request
+    local req = {
+        str_upper(params.method),
+        " ",
+        params.path,
+        query,
+        HTTP[version],
+        -- Pre-allocate slots for minimum headers and carriage return.
+        true,
+        true,
+        true,
+    }
+    local c = 6  -- req table index it's faster to do this inline vs table.insert
+
+    -- Append headers
+    for key, values in pairs(headers) do
+        if type(values) ~= "table" then
+            values = {values}
+        end
+
+        key = tostring(key)
+        for _, value in pairs(values) do
+            req[c] = key .. ": " .. tostring(value) .. "\r\n"
+            c = c + 1
+        end
+    end
+
+    -- Close headers
+    req[c] = "\r\n"
+    return tbl_concat(req)
+end
+
+
+local function _receive_status(sock)
+    local line, err = sock:receive("*l")
+    if not line then
+        return nil, nil, nil, err
+    end
+    return tonumber(str_sub(line, 10, 12)), tonumber(str_sub(line, 6, 8)), str_sub(line, 14)
+end
+
+
+local function _receive_headers(sock)
+    local headers = http_headers.new()
+    repeat
+        local line, err = sock:receive("*l")
+        if not line then
+            return nil, err
+        end
+
+        for key, val in str_gmatch(line, "([^:%s]+):%s*(.+)") do
+            if headers[key] then
+                if type(headers[key]) ~= "table" then
+                    headers[key] = { headers[key] }
+                end
+                tbl_insert(headers[key], tostring(val))
+            else
+                headers[key] = tostring(val)
+            end
+        end
+    until str_find(line, "^%s*$")
+    return headers, nil
+end
+
+
+local function _chunked_body_reader(sock, default_chunk_size)
+    
+    
