f64dd0ad943ea75f4572bad6071b6fcada3e14c7
lindyang <462171852@qq.com>
2016-12-18 20:05:06 +0800

    db.lua

--- dam/common/db.lua
+++ dam/common/db.lua
@@ -1,11 +1,42 @@
+require("debug")
 local mysql = require("resty.mysql")
 local _M = { _VERSION = '1.0' }
 
 
-function _M.get_conn()
+function _M.get_conn(self)
     local db, err = mysql:new()
     if not db then
-        error(
+        error("failed to instantiate mysql: ", err)
+        return
+    end
+
+    db:set_timeout(1000)  -- 1 sec
+
+    local ok, err, errcode, sqlstate = db:connect{
+        host = "127.0.0.1",
+        port = 3306,
+        database = "beehive_test",
+        password = "",
+        max_packet_size = 1024 * 1024
+    }
+    if not ok then
+        error("failed to connect: ", err, ": ", errcode, " ", sqlstate)
+        return
+    end
+    log("connect to mysql.")
+
+    function db:close()
+        local ok, err = db:set_keepalive(
+            tonumber(ngx.var.MYSQL_CONN_IDLE_TIMEOUT),
+            tonumber(ngx.var.MYSQL_CONN_POOL_SIZE)
+        )
+        if not ok then
+            error(err)
+            return
+        end
+    end
+
+    return db
 end
 
 
--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ if [ -z $openresty ]; then
     echo "Can not find openresty"
     exit 1
 fi
-
+[ -d $pwd_/logs ] || mkdir $pwd_/logs
 CONF=${ENV:-dev}.conf
 openresty -p $pwd_/ -c conf/$CONF
 
